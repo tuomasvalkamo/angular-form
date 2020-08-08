52f2ffc4506bba1a21c42da386c639e11c62ab00
Tuomas Valkamo <valkamo.tuomas@gmail.com>
2020-08-08 19:07:44 +0300

    add footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavComponent } from './nav/nav.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { HomeComponent } from './home/home.component';
     AboutComponent,
     ContactComponent,
     HomeComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
